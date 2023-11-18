bbb419c2275be0905e751a00142ccd2d02fe2a58
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-17 23:05:56 -0500

    Ejercicio25.java

---
+++ EjerciciosPracticosProgramacionEstructurada/EjerciciosPracticos/src/Ejercicio25.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+public class Ejercicio25{
+    public static void main(String [] args ){
+        Scanner lector = new Scanner(System.in);
+
+        //declaramos las variables 
+        String contraseña;
+        String confirmacionContraseña;
+        boolean contieneLetra = false;
+        boolean contieneDigito = false;
+        char caracter ;
+
+        
+        while (true) {
+             
+        
+                //pedimos contraseña al usuario        
+            System.out.print("Ingrese una contraseña: ");
+            contraseña = lector.nextLine();
+            
+            //la condu
+            if (contraseña.length() < 6 || contraseña.length() > 10) {
+                System.out.println("La contraseña debe tener entre 6 y 10 caracteres.");
+                continue; 
+            }
+            
+           
+            
+            for (int i = 0; i < contraseña.length(); i++) {
+                caracter = contraseña.charAt(i);
+                
+                if ((caracter >= 'A' && caracter <= 'Z') || (caracter >= 'a' && caracter <= 'z')) {
+                    contieneLetra = true;
+                }
+                
+                if (caracter >= '0' && caracter <= '9') {
+                    contieneDigito = true;
+                }
+                
+                if (contieneLetra && contieneDigito) {
+                    break; 
+                }
+            }
+            
+            if (!contieneLetra || !contieneDigito) {
+                System.out.println("**La contraseña debe contener al menos una letra y un dígito**.");
+                continue; 
+            }
+            
+            break; 
+            }
+        
+        while (true) {
+            System.out.print("Confirme la contraseña: ");
+            confirmacionContraseña = lector.nextLine();
+            
+            if (!confirmacionContraseña.equals(contraseña)) {
+                System.out.println("**Las contraseñas no coinciden. Inténtelo nuevamente**.");
+                continue;
+            }
+            
+            break;
+        }
+        
+        System.out.println("Contraseña establecida correctamente.");
+        lector.close();
+    
+    }
+}
